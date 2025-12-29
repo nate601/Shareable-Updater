8e90c4e769e78828445f8c94e9c1a406d246fb61
Nathan Button <nathan@nate601.me>
2025-12-29 16:59:35 -0600

    Move token to outer scope

--- kavita.py
+++ kavita.py
@@ -8,28 +8,28 @@ load_dotenv(envFile)
 api_key = os.getenv("KAVITA_API_KEY")
 assert api_key, "No API key.   Check ENV files"
 
+
 def GetToken():
     auth_url = "https://kavita.n8.pub/api/Plugin/authenticate"
-    o = {"apiKey": api_key, "pluginName" : "sharablesUpdater"}
+    o = {"apiKey": api_key, "pluginName": "sharablesUpdater"}
     resp = httpx.post(auth_url, params=o)
     resp = resp.json()
     assert resp["token"], "Unable to fetch token"
     return resp["token"]
 
+
 def GetSharables():
-    token = GetToken()
-    # baseurl = "https://kavita.n8.pub/api/Series/currently-reading"
     baseurl = "https://kavita.n8.pub/api/Series/on-deck"
-    headers = {
-        "Authorization": f"Bearer {token}"
-    }
+    headers = {"Authorization": f"Bearer {TOKEN}"}
     resp = httpx.post(baseurl, headers=headers)
     resp = resp.json()
     retVal = []
     for title in resp:
-        retVal.append({"name":title["name"], "link": "about:blank"})
+        retVal.append({"name": title["name"], "link": "about:blank"})
     return retVal
 
 
+TOKEN = GetToken()
+
 if __name__ == "__main__":
-   print(GetSharables())
+    print(GetSharables())
