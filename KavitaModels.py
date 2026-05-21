612a0ddbb73f6212f23e335f6c34209a7cbd071c
Nathan Button <nathan@nate601.me>
2026-05-20 19:53:08 -0500

    Move parsing logic to Pydantic All parsing logic is moved to KavitaModels

---
+++ KavitaModels.py
@@ -0,0 +1,158 @@
+from dataclasses import dataclass
+from typing import Any
+import httpx
+from functools import cached_property
+from datetime import datetime
+from pydantic import (
+    BaseModel,
+    Field,
+    AliasChoices,
+    computed_field,
+    field_validator,
+    ValidationInfo,
+)
+from setec_secrets import GetVendorSecret
+
+API_KEY = GetVendorSecret("kavita", "api_key")
+
+# TODO: update python and use template literal?
+# TODO: move base url to getvendorsecret call so I can change it later
+ON_DECK_ENDPOINT = "https://kavita.n8.pub/api/Series/on-deck"
+AUTH_ENDPOINT = "https://kavita.n8.pub/api/Plugin/authenticate"
+SERIES_VOLUMES_ENDPOINT = "https://kavita.n8.pub/api/Series/volumes"
+SERIES_METADATA_ENDPOINT = "https://kavita.n8.pub/api/Series/metadata"
+SERIES_DATA_ENDPOINT = "https://kavita.n8.pub/api/Series/"
+SERIES_SEARCH_ENDPOINT = "https://kavita.n8.pub/api/Series/v2"
+
+
+def GetToken():
+    o = {"apiKey": API_KEY, "pluginName": "sharablesUpdater"}
+    resp = httpx.post(AUTH_ENDPOINT, params=o)
+    resp = resp.json()
+    assert resp["token"], "Unable to fetch token"
+    return resp["token"]
+
+
+TOKEN = GetToken()
+HEADERS = {"Authorization": f"Bearer {TOKEN}"}
+client = httpx.Client(headers=HEADERS)
+
+
+class Volume(BaseModel):
+    id: int
+    name: str | None
+    pages: int
+    pagesRead: int
+    wordCount: int
+
+    @computed_field
+    @cached_property
+    def completed(self) -> bool:
+        return self.pagesRead >= self.pages
+
+    @field_validator("name", mode="before")
+    @classmethod
+    def validate_name(cls, input: Any, info: ValidationInfo) -> str:
+        if not isinstance(info.context, dict):
+            raise ValueError("Volume context is passed as null!")
+        seriesName = info.context.get("seriesName")
+        if isinstance(input, str):
+            if input.isdigit():
+                if (
+                    int(input) >= 1
+                ):  # series has multiple volumes, but are numbered not named
+                    return f"{seriesName} - Book {input}"
+            elif input[1:].isdigit():  # single title series book has name of -100000
+                return seriesName
+            else:
+                return input
+        raise ValueError(input)
+
+
+class Tag(BaseModel):
+    id: int
+    title: str | None
+
+
+class SeriesMetadata(BaseModel):
+    summary: str = "No summary!"
+    tags: list[Tag]
+    webLinks: list[str]
+
+    @field_validator("webLinks", mode="before")
+    @classmethod
+    def validate_webLinks(cls, input: Any) -> list[str]:
+        if input is None:
+            return ["about:blank"]
+        if isinstance(input, str) and input != "":
+            return input.split(";")
+        return ["about:blank"]
+
+
+class Series(BaseModel):
+    id: int
+    name: str = Field(validation_alias=AliasChoices("localizedName", "name"))
+    pages: int
+    pagesRead: int
+    latestReadDate: datetime
+
+    @computed_field
+    @cached_property
+    def metadata(self) -> SeriesMetadata:
+        resp = client.get(SERIES_METADATA_ENDPOINT, params={"seriesId": self.id})
+        return SeriesMetadata.model_validate_json(resp.content)
+
+    @computed_field
+    @cached_property
+    def volumes(self) -> list[Volume]:
+        resp = client.get(SERIES_VOLUMES_ENDPOINT, params={"seriesId": self.id})
+        resp = resp.json()
+        return [
+            Volume.model_validate(k, context={"seriesName": self.name}) for k in resp
+        ]
+
+    @computed_field
+    @cached_property
+    def completed(self) -> bool:
+        return self.pagesRead >= self.pages
+
+
+def GetSeries(id: int) -> Series:
+    resp = client.get(SERIES_DATA_ENDPOINT + str(id))
+    return Series.model_validate_json(resp.content, by_alias=True)
+
+
+@dataclass
+class KavitaFilterStatement:
+    comparison: int
+    field: int
+    value: str
+
+    def GetAsFilterStatement(self) -> dict[str, Any]:
+        return self.__dict__
+
+
+def SearchSeries(sortFields: list[KavitaFilterStatement]) -> list[Series]:
+    o = {
+        "combination": 1,
+        "limitTo": 0,
+        "sortOptions": {
+            "isAscending": False,
+            "sortField": 7,
+        },  ## Sort by most recently read (with most recent series at the top)
+        "statements": [k.__dict__ for k in sortFields],
+    }
+    resp = client.post(SERIES_SEARCH_ENDPOINT, json=o)
+    assert resp.status_code == 200
+    resp = resp.json()
+    return [GetSeries(r.get("id")) for r in resp]
+
+
+def GetOnDeckSeries() -> list[Series]:
+    resp = client.post(ON_DECK_ENDPOINT)
+    resp = resp.json()
+    retVal = []
+    for title in resp:
+        curSeries = GetSeries(title["id"])
+        retVal.append(curSeries)
+    return retVal
--- kavita.py
+++ kavita.py
@@ -1,111 +1,11 @@
-from dataclasses import dataclass
-from datetime import datetime
-from pprint import pprint
-
-import httpx
-
-from setec_secrets import GetVendorSecret
-
-api_key = GetVendorSecret("kavita", "api_key")
-
-ON_DECK_ENDPOINT = "https://kavita.n8.pub/api/Series/on-deck"
-AUTH_ENDPOINT = "https://kavita.n8.pub/api/Plugin/authenticate"
-SERIES_VOLUMES_ENDPOINT = "https://kavita.n8.pub/api/Series/volumes"
-SERIES_METADATA_ENDPOINT = "https://kavita.n8.pub/api/Series/metadata"
-SERIES_DATA_ENDPOINT = "https://kavita.n8.pub/api/Series/"
-SERIES_SEARCH_ENDPOINT = "https://kavita.n8.pub/api/Series/v2"
-# TODO: update python and use template literal
-
-
-def GetToken():
-    o = {"apiKey": api_key, "pluginName": "sharablesUpdater"}
-    resp = httpx.post(AUTH_ENDPOINT, params=o)
-    resp = resp.json()
-    assert resp["token"], "Unable to fetch token"
-    return resp["token"]
-
-
-@dataclass
-class Volume:
-    id: int
-    name: str
-    pages: int
-    pages_read: int
-    completed: bool
-    word_count: int
-    last_read_date: datetime
-
-
-@dataclass
-class SeriesMetadata:
-    summary: str | None
-    tags: list[str]
-    link: str | None
-
-
-@dataclass
-class Series:
-    id: int
-    name: str
-    pages: int
-    pages_read: int
-    metadata: SeriesMetadata | None
-    volumes: list[Volume]
-    last_read_date: datetime
-
-    def __init__(self, id: int):
-        self.id = id
-        resp = httpx.get(SERIES_DATA_ENDPOINT + str(id), headers=HEADERS)
-        resp = resp.json()
-        self.name = resp.get("localizedName") or resp.get("name")
-        self.pages = resp.get("pages")
-        self.pages_read = resp.get("pagesRead")
-        self.metadata = self.GetSeriesMetadata()
-        self.volumes = self.GetSeriesVolumes() or []
-        self.last_read_date = datetime.fromisoformat(resp.get("latestReadDate"))
-
-    def GetSeriesMetadata(self):
-        resp = httpx.get(
-            SERIES_METADATA_ENDPOINT, headers=HEADERS, params={"seriesId": self.id}
-        )
-        metadataResp = resp.json()
-        return SeriesMetadata(
-            summary=metadataResp.get("summary") or "No summary",
-            link=metadataResp.get("webLinks", "about:blank").split(",")[0] or None,
-            tags=[tag.get("title") for tag in metadataResp.get("tags")] or [],
-        )
-
-    def GetSeriesVolumes(self) -> list[Volume]:
-        resp = httpx.get(
-            SERIES_VOLUMES_ENDPOINT, headers=HEADERS, params={"seriesId": self.id}
-        )
-        booksResponse = resp.json()
-        volumes = []
-        for book in booksResponse:
-            bookName = book.get("name")
-            if bookName.isdigit():
-                if len(booksResponse) > 1:
-                    bookName = f"{self.name} - Book {bookName}"
-                else:
-                    bookName = self.name
-            elif bookName[1:].isdigit():
-                bookName = self.name
-            lrds = [
-                datetime.fromisoformat(c.get("lastReadingProgress"))
-                for c in book.get("chapters")
-            ]
-            lrds.sort()
-            curVol = Volume(
-                id=book["id"],
-                name=bookName,
-                pages=book["pages"],
-                pages_read=book["pagesRead"],
-                completed=book["pagesRead"] >= book["pages"],
-                word_count=book["wordCount"],
-                last_read_date=max(lrds),
-            )
-            volumes.append(curVol)
-        return volumes
+from rich import print
+from KavitaModels import (
+    Series,
+    Volume,
+    KavitaFilterStatement,
+    SearchSeries,
+    GetOnDeckSeries,
+)
 
 
 def GetSharables():
@@ -114,79 +14,38 @@ def GetSharables():
     for serie in series:
         o = {
             "name": serie.name,
-            "link": serie.GetSeriesMetadata().link,
-            "summary": serie.GetSeriesMetadata().summary,
-            "completed": int(serie.pages) <= int(serie.pages_read),
-            "completion_progress": serie.pages_read / serie.pages,
+            "link": serie.metadata.webLinks[0],
+            "summary": serie.metadata.summary,
+            "completed": serie.completed,
+            "completion_progress": serie.pagesRead / serie.pages,
         }
         retVal.append(o)
     return retVal
 
 
 def GetReadSeries() -> list[Series]:
-    fss = [
-        FilterStatement(1, 20, "98"),  # series with 99% progress or greater
-        FilterStatement(0, 19, "1"),  ## Series in the "Books" collection
-    ]
-    return SearchSeries(fss)
+    return SearchSeries(
+        [
+            KavitaFilterStatement(1, 20, "98"),  # series with 99% progress or greater
+            KavitaFilterStatement(0, 19, "1"),  ## Series in the "Books" collection
+        ]
+    )
 
 
 def GetReadVolumes() -> list[Volume]:
     series = GetSeriesWithProgress()
-    vol = sorted(
-        [v for s in series for v in s.volumes if v.completed],
-        key=lambda p: p.name,
-    )
-    return sorted(vol, key=lambda p: p.last_read_date.date(), reverse=True)
+    vol = [v for s in series for v in s.volumes if v.completed]
+    return vol
 
 
 def GetSeriesWithProgress() -> list[Series]:
-    fss = [
-        FilterStatement(1, 20, "0"),  ## Series with >= 1 percent of progress
-        FilterStatement(0, 19, "1"),  ## Series in the "Books" collection
-    ]
-    return SearchSeries(fss)
+    return SearchSeries(
+        [KavitaFilterStatement(1, 20, "0"), KavitaFilterStatement(0, 19, "1")]
+    )
 
-
-@dataclass
-class FilterStatement:
-    comparison: int
-    field: int
-    value: str
-
-
-def SearchSeries(sortFields: list[FilterStatement]) -> list[Series]:
-    o = {
-        "combination": 1,
-        "limitTo": 0,
-        "sortOptions": {
-            "isAscending": False,
-            "sortField": 7,
-        },  ## Sort by most recently read (with most recent series at the top)
-        "statements": [k.__dict__ for k in sortFields],
-    }
-    resp = httpx.post(SERIES_SEARCH_ENDPOINT, headers=HEADERS, json=o)
-    assert resp.status_code == 200
-    resp = resp.json()
-    return [Series(r.get("id")) for r in resp]
-
-
-def GetOnDeckSeries() -> list[Series]:
-    resp = httpx.post(ON_DECK_ENDPOINT, headers=HEADERS)
-    resp = resp.json()
-    retVal = []
-    for title in resp:
-        curSeries = Series(title["id"])
-        retVal.append(curSeries)
-
-    return retVal
-
-
-TOKEN = GetToken()
-HEADERS = {"Authorization": f"Bearer {TOKEN}"}
 
 if __name__ == "__main__":
     print("###On Deck###")
-    pprint(GetOnDeckSeries())
+    print(GetOnDeckSeries())
     print("###Read Volumes###")
-    pprint(GetReadVolumes())
+    print(GetReadVolumes())
